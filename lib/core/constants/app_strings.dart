758a3ccb6b3eea17dc1cf090ea746f04bd3b4b68
Antor <antorhawlader50@gmail.com>
2026-04-23 02:54:33 +0600

    update screenshot

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -3,7 +3,7 @@ class AppStrings {
   static const String settingsTitle = 'Settings';
   static const String historyTitle = 'History';
   static const String saveSettings = 'Save Settings';
-  static const String missingApiKey = 'API Key nai. Settings e giye set koro.';
+  static const String missingApiKey = 'No API Key Found';
   static const String unsupportedProvider =
       'Provider ti akhon support kora hoyni.';
 }
